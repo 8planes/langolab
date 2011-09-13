cbb915daac550e63b159886a3573c36541643709
Adam Duston <aduston@gmail.com>
2011-09-13 14:48:51 -0400

    Adding more stuff to make watir tests run

--- puppet/modules/watir/manifests/init.pp
+++ puppet/modules/watir/manifests/init.pp
@@ -1,12 +1,15 @@
 class watir {
   package {
     "ruby": ensure => "latest";
+    "ruby-dev": ensure => "latest";
     "rubygems": ensure => "latest";
+    "libopenssl-ruby": ensure => "latest";
+    "firefox": ensure => "latest";
   }
 
   package { "watir-webdriver":
     ensure => "latest",
     provider => gem,
-    require => Package["ruby"];
+    require => [Package[ruby], Package[ruby-dev], Package[rubygems]];
   }
 }
