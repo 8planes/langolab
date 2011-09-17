ce1cffeac938eac6e17aabd4296682495def6997
Adam Duston <aduston@gmail.com>
2011-09-17 13:55:28 -0400

    Removed admin stuff from rabbitmqctl, which is unsupported in rabbitmq-server in ubuntu 10.04 repos

--- puppet/modules/rabbitmq/lib/puppet/type/rabbitmq_user.rb
+++ puppet/modules/rabbitmq/lib/puppet/type/rabbitmq_user.rb
@@ -16,19 +16,8 @@ Puppet::Type.newtype(:rabbitmq_user) do
     newvalues(/^\S+$/)
   end
 
-  # newproperty(:password) do
   newparam(:password) do
     desc 'User password to be set *on creation*'
-  end
-
-  newproperty(:admin) do
-    desc 'rather or not user should be an admin'
-    newvalues(/true|false/)
-    munge do |value|
-      # converting to_s incase its a boolean
-      value.to_s.to_sym
-    end
-    defaultto :false
   end
 
   validate do
