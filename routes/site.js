63a225bfc252874ed16ce92f27db026a12aeed04
Adam Duston <aduston@gmail.com>
2012-02-12 23:35:02 -0500

    Adding login now

--- routes/site.js
+++ routes/site.js
@@ -1,5 +1,14 @@
 var ConversationStat = require('../models/conversationstat');
 
+function ensureUser(req, res, next) {
+    if (req.user) {
+        next();
+    }
+    else {
+        res.redirect('/login?next=' + encodeURIComponent(req.url))
+    }
+}
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
         ConversationStat.topForLastWeek(5, function(stats) {
@@ -7,5 +16,11 @@ module.exports = function(app) {
                 conversationStats: stats
             });
         });
+    });
+    app.get('/login', function(req, res) {
+        res.render('login');
+    });
+    app.get('/enter_conversations', ensureUser, function(req, res) {
+        
     });
 };
